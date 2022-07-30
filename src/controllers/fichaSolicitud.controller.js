9a4273db8d47749b9658a2f2582b547b53c7958e
mherrera 2020235 <88869102+MarckHerrera@users.noreply.github.com>
2022-07-30 11:06:38 -0600

    Revert "Validaciones finalizadas"
    This reverts commit b28a3ea03261cf4fcb36706a4d0d91709ad10b36.

--- src/controllers/fichaSolicitud.controller.js
+++ src/controllers/fichaSolicitud.controller.js
@@ -9,17 +9,6 @@ function agregarFicha(req, res) {
         && parametros.telefono && parametros.fechaNacimiento
         && parametros.carrera && parametros.generoDePoesia) {
 
-        const diaPedido = new Date(Date.now());
-
-        modeloFicha.nombre = parametros.nombre;
-        modeloFicha.carnet = parametros.carnet;
-        modeloFicha.direccion = parametros.direccion;
-        modeloFicha.genero = parametros.genero;
-        modeloFicha.telefono = parametros.telefono;
-        modeloFicha.fechaNacimiento = parametros.fechaNacimiento;
-        modeloFicha.carrera = parametros.carrera;
-        modeloFicha.generoDePoesia = parametros.generoDePoesia;
-        modeloFicha.fechaDeInscripción = diaPedido;
 
 
 
@@ -32,135 +21,134 @@ function agregarFicha(req, res) {
                         return res.status(500).send({ mensaje: "Carnet ya utilizado" });
                     } else {
 
-                        const nacimiento = new Date(parametros.fechaNacimiento);
-                        const años = diaPedido.getFullYear() - nacimiento.getFullYear();
-                        if (años < 17) {
-                            return res.status(400).send({ mensaje: "Debe de tener mas de 17 años" });
+                        const letraA = parametros.carnet.slice(0, 1);
+
+                        if (letraA != "A" && letraA != "a") {
+                            return res.status(500).send({ mensaje: "El carnet debe tener una A o a, al principio" });
                         } else {
 
-                            const tercer5 = parametros.carnet.slice(2, 3);
-
-                            if (tercer5 !== "5") {
-                                return res.status(400).send({ mensaje: "El tercer digito del carnet debe ser 5" });
-                              }else{
-
-                            const letraA = parametros.carnet.slice(0, 1);
-
-                            if (letraA != "A" && letraA != "a") {
-                                return res.status(500).send({ mensaje: "El carnet debe tener una A o a, al principio" });
+                        if (parametros.carnet.length != 6) {
+                            return res.status(500).send({ mensaje: "El carnet debe de tener 6 digitos" });
+                        } else {
+                            if (parametros.carnet.split("")[parametros.carnet.length - 1] != 1 &&
+                                parametros.carnet.split("")[parametros.carnet.length - 1] != 3 &&
+                                parametros.carnet.split("")[parametros.carnet.length - 1] != 9 ) {
+                                return res.status(500).send({ mensaje: "el ultimo digito del carnet debe de ser 1, 3 o 9" });
                             } else {
+                                
+                            
 
-                                if (parametros.carnet.length != 6) {
-                                    return res.status(500).send({ mensaje: "El carnet debe de tener 6 digitos" });
-                                } else {
-                                    if (parametros.carnet.split("")[parametros.carnet.length - 1] != 1 &&
-                                        parametros.carnet.split("")[parametros.carnet.length - 1] != 3 &&
-                                        parametros.carnet.split("")[parametros.carnet.length - 1] != 9) {
-                                        return res.status(500).send({ mensaje: "el ultimo digito del carnet debe de ser 1, 3 o 9" });
-                                    } else {
+                                const diaPedido = new Date(Date.now());
 
+                                modeloFicha.nombre = parametros.nombre;
+                                modeloFicha.carnet = parametros.carnet;
+                                modeloFicha.direccion = parametros.direccion;
+                                modeloFicha.genero = parametros.genero;
+                                modeloFicha.telefono = parametros.telefono;
+                                modeloFicha.fechaNacimiento = parametros.fechaNacimiento;
+                                modeloFicha.carrera = parametros.carrera;
+                                modeloFicha.generoDePoesia = parametros.generoDePoesia;
+                                modeloFicha.fechaDeInscripción = diaPedido;
 
+                                function calculaEntregaFines(diaPedido, diasPactados,) {
 
-                                        function calculaEntregaFines(diaPedido, diasPactados,) {
+                                    let diaPropuesto = new Date(diaPedido.getFullYear(), diaPedido.getMonth(), diaPedido.getDate());
 
-                                            let diaPropuesto = new Date(diaPedido.getFullYear(), diaPedido.getMonth(), diaPedido.getDate());
+                                    let i = 1;
 
-                                            let i = 1;
+                                    while (diasPactados > 0) {
 
-                                            while (diasPactados > 0) {
+                                        diaPropuesto = new Date(diaPedido.getFullYear(), diaPedido.getMonth(), diaPedido.getDate() + i);
 
-                                                diaPropuesto = new Date(diaPedido.getFullYear(), diaPedido.getMonth(), diaPedido.getDate() + i);
-
-                                                if (diaPropuesto.getDay() > 0 && diaPropuesto.getDay() < 6) {
-                                                    diasPactados--;
-                                                }
-                                                i++;
-                                            }
-
-                                            return diaPropuesto;
-
+                                        if (diaPropuesto.getDay() > 0 && diaPropuesto.getDay() < 6) {
+                                            diasPactados--;
                                         }
-
-                                        function calculaEntregaFinMes(diaPedido, diasPactados,) {
-
-                                            let diaPropuesto = new Date(diaPedido.getFullYear(), diaPedido.getMonth() + 1, 0);
-
-                                            let i = 1;
-
-                                            while (diasPactados == 0) {
-
-                                                diaPropuesto = new Date(diaPedido.getFullYear(), diaPedido.getMonth() + 1, 0 - i);
-
-                                                if (diaPropuesto.getDay() > 0 && diaPropuesto.getDay() < 6) {
-                                                    diasPactados--;
-                                                }
-                                                i++;
-                                            }
-
-                                            return diaPropuesto;
-
-                                        }
-
-                                        if (parametros.carnet.split("")[parametros.carnet.length - 1] == 1 && parametros.generoDePoesia == "Dramático") {
-
-                                            const diaEntrega = calculaEntregaFines(diaPedido, 5,);
-
-                                            modeloFicha.fechaDeDeclamacion = diaEntrega;
-
-
-
-                                            modeloFicha.save((err, fichaGuardada) => {
-                                                if (err) return res.status(500).send({ mensaje: "Error en la peticion" });
-                                                if (!fichaGuardada) return res.status(500).send({ mensaje: "Error al registrar Ficha" });
-
-                                                return res.status(200).send({ ficha: fichaGuardada });
-                                            })
-
-                                        }
-
-                                        if (parametros.carnet.split("")[parametros.carnet.length - 1] == 3 && parametros.generoDePoesia == "Épico") {
-
-                                            const diaEntrega = calculaEntregaFinMes(diaPedido, 0,);
-
-                                            modeloFicha.fechaDeDeclamacion = diaEntrega;
-
-
-                                            modeloFicha.save((err, fichaGuardada) => {
-                                                if (err) return res.status(500).send({ mensaje: "Error en la peticion" });
-                                                if (!fichaGuardada) return res.status(500).send({ mensaje: "Error al registrar Ficha" });
-
-                                                return res.status(200).send({ ficha: fichaGuardada });
-                                            })
-
-                                        } else {
-
-
-                                            var viernes = new Date(diaPedido.getFullYear(), diaPedido.getMonth(), diaPedido.getDate());
-                                            var Nday = (viernes.getDay() == 0) ? 7 : viernes.getDay();
-                                            var SumDay = 7 - Nday;
-                                            viernes.setDate(viernes.getDate() + SumDay + 5);
-
-                                            modeloFicha.fechaDeDeclamacion = viernes;
-
-
-                                            modeloFicha.save((err, fichaGuardada) => {
-                                                if (err) return res.status(500).send({ mensaje: "Error en la peticion" });
-                                                if (!fichaGuardada) return res.status(500).send({ mensaje: "Error al registrar Ficha" });
-
-                                                return res.status(200).send({ ficha: fichaGuardada });
-                                            })
-
-
-                                        }
-
-
-                                        /* sdasda */
+                                        i++;
                                     }
+
+                                    return diaPropuesto;
+
                                 }
-                            }
+
+                                function calculaEntregaFinMes(diaPedido, diasPactados,) {
+
+                                    let diaPropuesto = new Date(diaPedido.getFullYear(), diaPedido.getMonth() + 1, 0);
+
+                                    let i = 1;
+
+                                    while (diasPactados == 0) {
+
+                                        diaPropuesto = new Date(diaPedido.getFullYear(), diaPedido.getMonth() + 1, 0 - i);
+
+                                        if (diaPropuesto.getDay() > 0 && diaPropuesto.getDay() < 6) {
+                                            diasPactados--;
+                                        }
+                                        i++;
+                                    }
+
+                                    return diaPropuesto;
+
+                                }
+
+                                if (parametros.carnet.split("")[parametros.carnet.length - 1] == 1 && parametros.generoDePoesia == "Dramático") {
+
+                                    const diaEntrega = calculaEntregaFines(diaPedido, 5,);
+
+                                    modeloFicha.fechaDeDeclamacion = diaEntrega;
+
+
+
+                                    modeloFicha.save((err, fichaGuardada) => {
+                                        if (err) return res.status(500).send({ mensaje: "Error en la peticion" });
+                                        if (!fichaGuardada) return res.status(500).send({ mensaje: "Error al registrar Ficha" });
+
+                                        return res.status(200).send({ ficha: fichaGuardada });
+                                    })
+
+                                }
+
+                                if (parametros.carnet.split("")[parametros.carnet.length - 1] == 3 && parametros.generoDePoesia == "Épico") {
+
+                                    const diaEntrega = calculaEntregaFinMes(diaPedido, 0,);
+
+                                    modeloFicha.fechaDeDeclamacion = diaEntrega;
+
+
+                                    modeloFicha.save((err, fichaGuardada) => {
+                                        if (err) return res.status(500).send({ mensaje: "Error en la peticion" });
+                                        if (!fichaGuardada) return res.status(500).send({ mensaje: "Error al registrar Ficha" });
+
+                                        return res.status(200).send({ ficha: fichaGuardada });
+                                    })
+
+                                }
+                                else {
+
+
+                                    var viernes = new Date(diaPedido.getFullYear(), diaPedido.getMonth(), diaPedido.getDate());
+                                    var Nday = (viernes.getDay() == 0) ? 7 : viernes.getDay();
+                                    var SumDay = 7 - Nday;
+                                    viernes.setDate(viernes.getDate() + SumDay + 5);
+
+                                    modeloFicha.fechaDeDeclamacion = viernes;
+
+
+                                    modeloFicha.save((err, fichaGuardada) => {
+                                        if (err) return res.status(500).send({ mensaje: "Error en la peticion" });
+                                        if (!fichaGuardada) return res.status(500).send({ mensaje: "Error al registrar Ficha" });
+
+                                        return res.status(200).send({ ficha: fichaGuardada });
+                                    })
+
+
+                                }
+
+                            
+                            /* sdasda */
                         }
                     }
                     }
+                }
                 })
 
             }
